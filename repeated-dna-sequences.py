233215e60ecc7f63571148b761edfebb5cdb9ce7
roma <romhedoo@gmail.com>
2019-04-17 13:45:29 +0200

    faster than 95%

--- repeated-dna-sequences.py
+++ repeated-dna-sequences.py
@@ -1,10 +1,7 @@
 class Solution:
-    def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        #if len(s) == 10: 
-            #return [s]
-        #else: 
+    def findRepeatedDnaSequences(self, s: str) -> List[str]: 
         all_sub = [] 
-        for i in range(0, (len(s)-9)):
+        for i in range(0, (len(s)-8+1)):
             sub = s[i:i+10]
             all_sub.append(sub)
         unique_sub = set(all_sub)
@@ -13,3 +10,16 @@ class Solution:
             if s.count(sub) > 1: 
                 answer.append(sub)
         return answer
+
+
+class Solution: #faster than 95%
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        answer = set()
+        all_sub = set()
+        for i in range(len(s)-10+1):
+            sub = s[i:i+10] #a substring
+            if sub in all_sub: #if already found --add to answer
+                answer.add(sub) #method for sets, not lists
+            else:
+                all_sub.add(sub) #add to found
+        return list(answer)
